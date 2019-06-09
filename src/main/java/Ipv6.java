910df7eb15e15fafc0925804fe1ae561202af90b
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2019-06-09 14:29:32 -0300

    Transformação em projeto Maven

---
+++ src/main/java/Ipv6.java
@@ -0,0 +1,26 @@
+public class Ipv6 {
+
+    private final byte[] packet;
+
+    private final int version;
+
+    public Ipv6(final byte[] packet) {
+        this.packet = packet;
+//        this.version = obtainVersion();
+        this.version = 0;
+    }
+
+    private int obtainVersion() {
+        return getFirstNBits(packet[1], 4);
+    }
+
+    private int getFirstNBits(final byte bits, final int n) {
+        final int mask = 1<<n-1;
+        return bits & mask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Ipv6(null).getFirstNBits((byte) 51, 3));
+    }
+
+}
